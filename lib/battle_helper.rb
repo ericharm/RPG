9bcbda46ea40aebd1cd312e2f0b4bffd5a326142
Eric E. Harm <ericharm@gmail.com>
2013-10-05 04:41:59 -0400

    Complete turn-based battle system.

--- lib/battle_helper.rb
+++ lib/battle_helper.rb
@@ -8,17 +8,39 @@ module BattleHelper
   end
 
   def zap
-
+    if @player.mp < 5
+      puts "You don't have enough MP."
+      battle_menu
+    else
+      @player.change_stat(:mp, 5, :subtract)
+      dice_count = @player_class == "Mage" ? 3 : 2
+      bonus_damage = roll_dice(dice_count)
+      @player.change_stat(:attack, bonus_damage)
+      puts "ZAP!"
+      hit(@player, @monster)
+      @player.change_stat(:attack, bonus_damage, :subtract)
+      hit(@monster, @player) unless @monster.dead?
+      check_for_fatal_blow
+      battle_menu unless @player.dead? || @monster.dead?
+    end
   end
 
   def charge
-
+    puts "\n#{@player.name} be Charging. . .\n"
+    2.times { hit(@monster, @player) }
+    check_for_fatal_blow
+    #decide on an amount of times based on class
+    dice_count = @player_class == "Warrior" ? 3 : 2
+    p = roll_dice(dice_count) - dice_count
+    puts "\n#{@player.name} unleashes #{p} strikes!"
+    p.times { hit(@player, @monster) }
+    check_for_fatal_blow
+    battle_menu unless @first_striker.dead? || @second_striker.dead?
   end
 
   def run
     dice_count = @player.player_class == "Rogue" ? 3 : 2
     p = roll_dice(dice_count)
-    puts p
     if p < 7
       puts "Failed to escape."
       hit(@monster, @player)
@@ -43,13 +65,6 @@ module BattleHelper
       puts "#{@player.name} has been killed!"
       puts "\n > > >  You have died!  < < <\n"
       Game.game_over
-    end
-  end
-
-  def fight
-    until @first_striker.dead? || @second_striker.dead?
-      hit(@first_striker, @second_striker) 
-      hit(@second_striker, @first_striker) unless @second_striker.dead?
     end
   end
 
@@ -91,5 +106,4 @@ module BattleHelper
       return false
     end
   end
-
 end
